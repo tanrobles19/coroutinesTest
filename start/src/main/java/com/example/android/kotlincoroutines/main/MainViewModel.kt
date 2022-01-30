526458d6c904598a5c23be0099d6882bb36fafcc
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-01-29 23:22:28 -0600

    adding coroutines to viewmodel and repository

--- start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -124,17 +124,31 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
     fun refreshTitle() {
-        // TODO: Convert refreshTitle to use coroutines
-        _spinner.value = true
-        repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
-            override fun onCompleted() {
+
+        viewModelScope.launch {
+            try {
+                _spinner.value = true
+                repository.refreshTitle()
+            }catch (error: TitleRefreshError) {
+                _snackBar.postValue(error.message)
+                _spinner.postValue(false)
+            }finally {
                 _spinner.postValue(false)
             }
 
-            override fun onError(cause: Throwable) {
-                _snackBar.postValue(cause.message)
-                _spinner.postValue(false)
-            }
-        })
+        }
+
+//        // TODO: Convert refreshTitle to use coroutines
+//        _spinner.value = true
+//        repository.refreshTitleWithCallbacks(object : TitleRefreshCallback {
+//            override fun onCompleted() {
+//                _spinner.postValue(false)
+//            }
+//
+//            override fun onError(cause: Throwable) {
+//                _snackBar.postValue(cause.message)
+//                _spinner.postValue(false)
+//            }
+//        })
     }
 }
--- start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -19,6 +19,9 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -44,35 +47,44 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
 
     // TODO: Add coroutines-based `fun refreshTitle` here
 
-    /**
-     * Refresh the current title and save the results to the offline cache.
-     *
-     * This method does not return the new title. Use [TitleRepository.title] to observe
-     * the current tile.
-     */
-    fun refreshTitleWithCallbacks(titleRefreshCallback: TitleRefreshCallback) {
-        // This request will be run on a background thread by retrofit
-        BACKGROUND.submit {
-            try {
-                // Make network request using a blocking call
-                val result = network.fetchNextTitle().execute()
-                if (result.isSuccessful) {
-                    // Save it to database
-                    titleDao.insertTitle(Title(result.body()!!))
-                    // Inform the caller the refresh is completed
-                    titleRefreshCallback.onCompleted()
-                } else {
-                    // If it's not successful, inform the callback of the error
-                    titleRefreshCallback.onError(
-                            TitleRefreshError("Unable to refresh title", null))
-                }
-            } catch (cause: Throwable) {
-                // If anything throws an exception, inform the caller
-                titleRefreshCallback.onError(
-                        TitleRefreshError("Unable to refresh title", cause))
+    suspend fun refreshTitle() {
+        withContext(Dispatchers.IO) {
+            val result = try {
+                network.fetchNextTitle().execute()
+            }catch (cause: Throwable) {
+                throw TitleRefreshError("Unable to refresh the title", cause)
+            }
+            if(result.isSuccessful) {
+                titleDao.insertTitle(Title(result.body()!!  + "_"))
+            }else{
+                throw TitleRefreshError("Unable to refresh the title", null)
             }
         }
     }
+
+//    fun refreshTitleWithCallbacks(titleRefreshCallback: TitleRefreshCallback) {
+//        // This request will be run on a background thread by retrofit
+//        BACKGROUND.submit {
+//            try {
+//                // Make network request using a blocking call
+//                val result = network.fetchNextTitle().execute()
+//                if (result.isSuccessful) {
+//                    // Save it to database
+//                    titleDao.insertTitle(Title(result.body()!!))
+//                    // Inform the caller the refresh is completed
+//                    titleRefreshCallback.onCompleted()
+//                } else {
+//                    // If it's not successful, inform the callback of the error
+//                    titleRefreshCallback.onError(
+//                            TitleRefreshError("Unable to refresh title", null))
+//                }
+//            } catch (cause: Throwable) {
+//                // If anything throws an exception, inform the caller
+//                titleRefreshCallback.onError(
+//                        TitleRefreshError("Unable to refresh title", cause))
+//            }
+//        }
+//    }
 }
 
 /**
